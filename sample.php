e76cd681bc71ccb8cafdf916a06cfd42f66809ae
sho-19980909 <sho.1243@icloud.com>
2021-08-01 22:42:04 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html　lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじの結果</h1>
+       <p>おみくじの結果:なし</p>
+       <form action="sample.php" method="POST">
+           <button type="submit" name="act" value="draw">おみくじを引く！</button>
+        <form>
+    </body>
+</html>
